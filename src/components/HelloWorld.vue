d9f6e4fd70dbabbb769e0f0e4754b4142f7c2a87
Erik <eabramczyk@mxtrautomation.com>
2019-02-05 23:55:29 -0500

    functioning

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,47 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p>{{ message }}</p>
+    <p>I will add {{number1}} to {{number2}} on the server</p>
+
+    <p>Server Response Message: {{messageResponse}}</p>
+    <p>Server Response Add: {{addResponse}}</p>
+
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+   private message: string = "I will send this to the server";
+   private number1: number = 2;
+   private number2: number = 5
+
+   private messageResponse = "";
+   private addResponse = ""
+   mounted() {
+     let key: string = "?code=KTtarDLQ/PovFhwXsaa4GX7D6jg8zS7z9YDlRy2UQNV33NgCUeCZsg==";
+     let addNumsURL = "https://funder-test-functions.azurewebsites.net/api/addTwoNumbers";
+     let messageURL = "https://funder-test-functions.azurewebsites.net/api/messageReturn";
+     axios.post(messageURL + key, {"message": this.message}).then(result=>{
+       console.log('success!');
+       this.messageResponse = result.data;
+     }, reason => {
+       console.log('Message failure: ' + reason)
+     })
+     axios.post(addNumsURL + key, {"number_one": this.number1, "number_two": this.number2}).then(result=>{
+       console.log('success!');
+       this.addResponse = result.data;
+     }, reason =>{
+       console.log('Add failure: ' + reason);
+     })
+   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
